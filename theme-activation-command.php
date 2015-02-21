30383146261386be95bb549fd2c8f09dde005b0c
Ben Word <ben@benword.com>
2015-02-20 18:44:08 -0600

    Rename command, use short array syntax

--- theme-activation-command.php
+++ theme-activation-command.php
@@ -8,12 +8,12 @@ if (!defined('WP_CLI')) {
 
 class RootsThemeActivationCommand extends \WP_CLI_Command {
   /**
-   * Roots Theme Activation options
+   * Theme activation options
    *
    * ## OPTIONS
    *
    * <theme>
-   * : The theme name to activate. Defaults to 'roots'.
+   * : The theme name to activate. Defaults to 'sage'.
    *
    * [--show-on-front=<page-type>]
    * : What to show on the front page. Options are: 'posts', 'page'. Default is 'page'.
@@ -26,50 +26,50 @@ class RootsThemeActivationCommand extends \WP_CLI_Command {
    *
    * ## EXAMPLES
    *
-   *     wp roots options
-   *     wp roots options --show_on_front=page --permalink_structure='/%year%/%postname%/' --skip_navigation
+   *     wp theme-activation options
+   *     wp theme-activation options --show_on_front=page --permalink_structure='/%year%/%postname%/' --skip_navigation
    *
    */
   public function options($args, $options) {
     list($name) = $args;
 
-    $defaults = array(
+    $defaults = [
       'permalink_structure' => '/%postname%/',
       'show_on_front'       => 'page',
       'skip_navigation'     => null,
-      'theme'               => 'roots'
-    );
+      'theme'               => 'sage'
+    ];
 
     $options = wp_parse_args($options, $defaults);
 
     WP_CLI::log('Activating theme and setting options');
 
-    $home_page_options = array(
+    $home_page_options = [
       'post_content' => 'Lorem Ipsum',
       'post_status'  => 'publish',
       'post_title'   => 'Home',
       'post_type'    => 'page',
       'porcelain'    => true
-    );
+    ];
 
-    WP_CLI::run_command(array('theme', 'activate', $option['theme']));
+    WP_CLI::run_command(['theme', 'activate', $option['theme']]);
 
     $home_page_id = WP_CLI::run_command(array('post', 'create'), $home_page_options);
 
-    WP_CLI::run_command(array('option', 'update', 'show_on_front', $option['show_on_front']));
-    WP_CLI::run_command(array('option', 'update', 'page_on_front', $home_page_id));
+    WP_CLI::run_command(['option', 'update', 'show_on_front', $option['show_on_front']]);
+    WP_CLI::run_command(['option', 'update', 'page_on_front', $home_page_id]);
 
-    WP_CLI::run_command(array('rewrite', 'structure', $option['permalink_structure']));
-    WP_CLI::run_command(array('rewrite', 'flush'));
+    WP_CLI::run_command(['rewrite', 'structure', $option['permalink_structure']]);
+    WP_CLI::run_command(['rewrite', 'flush']);
 
     if (!empty($option['skip_navigation'])) {
-      WP_CLI::run_command(array('menu', 'create', 'Primary Navigation'));
-      WP_CLI::run_command(array('menu', 'location', 'assign', 'primary_navigation'));
-      WP_CLI::run_command(array('menu', 'item', 'add-post', 'Primary Navigation', $home_page_id));
+      WP_CLI::run_command(['menu', 'create', 'Primary Navigation']);
+      WP_CLI::run_command(['menu', 'location', 'assign', 'primary_navigation']);
+      WP_CLI::run_command(['menu', 'item', 'add-post', 'Primary Navigation', $home_page_id]);
     }
 
     WP_CLI::success('Theme activated');
   }
 }
 
-\WP_CLI::add_command('roots', new RootsThemeActivationCommand);
+\WP_CLI::add_command('theme-activation', new RootsThemeActivationCommand);
